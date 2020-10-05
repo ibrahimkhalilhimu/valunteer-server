9e67860a38332fb39166cd1b7fd27df976001c8f
ibrahimkhalilhimu <ibrahimkhalilhemu1@gmail.com>
2020-10-06 00:16:23 +0600

    done

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.huwqv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 app.use(cors())
 const port = 5000
 
